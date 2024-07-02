6d9abb7713515f98baf8a4c8cc1d159f90fd65c1
rubyej27 <ruby_jackson1@my.vcccd.edu>
2024-07-01 21:40:31 -0400

    commit

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import random
 
 
 def findEvenNumber(numbers):
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
-
+    for num in numbers[1:]:
+        if num % 2 == 0:
+            yield (num)
+           
 
 def main():
     numbers = [random.randint(0, 100) for i in range(10)]
